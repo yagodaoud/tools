c44747296b4c4789eb1ab92d00320adadedc7766
Yago Andrade <yagodaoud@hotmail.com>
2023-03-09 15:24:56 -0300

    Added Emoji Dict

--- Menu.py
+++ Menu.py
@@ -4,19 +4,203 @@ beans = [""]
 meats = [""]
 sides = [""]
 salads = ["\n🥬Alface", "🍅Tomate", "🥬Rúcula", "🍅Vinagrete", "🍍Abacaxi", "🥭Manga", "🥕Cenoura", "🍠Beterraba", "🥒Pepino"]
-sections = [rice, beans, meats, sides, salads]
+sections = {"rice": rice, "beans": beans, "meats": meats, "sides": sides, "salads": salads}
+
+emoji_map = {
+    "Arroz": "🍚",
+    "Arroz Integral": "🍚",
+    "Arroz de Forno": "🍚",
+    "Arroz Temperado": "🍚",
+    "Galinhada": "🐓",
+    #########################
+
+
+
+    "Feijão": "🥘",
+    "Feijão Preto": "🥘",
+    "Feijão Tropeiro": "🥘",
+    "Feijão Branco": "🥘",
+    "Feijoada": "🥘",
+    "Tutu de Feijão": "🥘",
+    #########################
+
+
+
+    "Bife Acebolado": "🥩",
+    "Carne Moída com Legumes": "🥩",
+    "Carne Moída": "🥩",
+    "Hambúrguer à Pizzaiolo": "🥩",
+    "Ponta de Peito": "🥩",
+    "Ponta de Peito ao Alho": "🥩",
+    "Carne Bovina Assada": "🥩",
+    "Carne Bovina Grelhada": "🥩",
+    "Carne Bovina ao Molho": "🥩",
+    "Almôndegas Fritas": "🥩",
+    "Almôndegas ao Molho": "🥩",
+    "Rabada": "🥩",
+    "Strogonoff Bovino": "🥩",
+    "Tiras de Costela": "🥩",
+    "Ripa de Costela": "🥩",
+    "Bife de Panela": "🥩",
+    "Carne de Panela": "🥩",
+    "Picadinho Bovino": "🥩",
+    "Picadinho Bovino ao Molho": "🥩",
+    "Picadinho Bovino ao Molho Madeira": "🥩",
+    "Iscas Bovinas": "🥩",
+    #########################
+
+    "Frango Grelhado": "🐓",
+    "Frango Frito": "🐓",
+    "Frango Assado": "🐓",
+    "Frango ao Molho": "🐓",
+    "Frango Caipira": "🐓",
+    "Strogonoff de Frango": "🐓",
+    "Frango à Passarinho": "🐓",
+    "Frango Empanado": "🐓",
+    "Picadinho de Frango": "🐓",
+    "Pipoca de Frango Empanado": "🐓",
+    "Iscas de Frango Empanadas": "🐓",
+    #########################
+
+    "Carne Suína Assada": "🍖",
+    "Carne Suína Grelhada": "🍖",
+    "Carne Suína ao Molho": "🍖",
+    "Lombo Grelhado": "🍖",
+    "Lombo Assado": "🍖",
+    "Lombo ao Molho": "🍖",
+    "Lombo ao Molho de Alho": "🍖",
+    "Bife Suíno": "🍖",
+    "Bife Suíno Acebolado": "🍖",
+    "Pernil Assado": "🍖",
+    "Pernil de Panela": "🍖",
+    "Picadinho Suíno": "🍖",
+    "Picadinho Suíno ao Molho Madeira": "🍖",
+    #########################
+
+
+
+    "Batata Frita": "🍟",
+    "Batata Assada": "🥔",
+    "Batata ao Molho": "🥔",
+    "Batata Rústica": "🥔",
+    "Batata com Ervas": "🥔",
+    "Batata Gratinada": "🥔",
+    "Batata Chips": "🥔",
+    #########################
+
+    "Batata Doce": "🍠",
+    "Batata Doce Frita": "🍠",
+    "Batata Doce Chips": "🍠",
+    "Batata Doce Cozida": "🍠",
+    #########################
+
+    "Couve Refogada": "🥦",
+    "Couve com Bacon": "🥦",
+    "Abobrinha Refogada": "🥦",
+    "Espaguete de Abobrinha": "🥦",
+    "Virado de Abobrinha": "🥦",
+    "Abobrinha ao Molho": "🥦",
+    "Quiabo Refogado": "🥦",
+    "Quiabo Frito": "🥦",
+    "Chuchu Refogado": "🥦",
+    "Mix de Legumes": "🥦",
+    "Legumes na Manteiga": "🥦",
+    "Jiló Frito": "🥦",
+    "Jiló Refogado": "🥦",
+    "Virado de Jiló": "🥦",
+    "Couve-flor Refogada": "🥦",
+    "Couve-flor Gratinada": "🥦",
+    "Couve-flor Empanada": "🥦",
+    "Brócolis Refogado": "🥦",
+    "Repolho Refogado": "🥦",
+    "Vagem Refogada": "🥦",
+    #########################
+    
+    "Berinjela Refogada": "🍆",
+    "Berinjela da Casa": "🍆",
+    "Berinjela Frita": "🍆",
+    "Berinjela à Parmegiana": "🍆",
+    "Berinjela à Parmegiana(sem Presunto)": "🍆",
+    #########################
+
+    "Banana Frita": "🍌",
+    "Banana Empanada": "🍌",
+    "Farofa de Banana": "🍌",
+    "Banana Flambada": "🍌",
+    #########################
+
+    "Nhoque": "🥔",
+    "Nhoque ao Sugo": "🥔",
+    "Nhoque ao Molho Rosé": "🥔",
+    "Nhoque ao Molho": "🥔",
+    #########################
+
+    "Macarrão": "🍝",
+    "Macarrão Alho e Óleo": "🍝",
+    "Macarrão de Forno": "🍝",
+    "Macarrão ao Molho Rosé": "🍝",
+    "Macarrão com Brócolis": "🍝",
+    "Macarrão com Brócolis e Bacon": "🍝",
+    "Macarrão ao Sugo": "🍝",
+    #########################
+
+    "Ovos Fritos": "🍳",
+    "Omelete": "🍳",
+    "Omelete de Forno": "🍳",
+    "Omelete Recheado": "🍳",
+    #########################
+
+    "Cenoura Refogada": "🥕",
+    "Cenoura Cozida": "🥕",
+    "Cenoura com Bacon": "🥕",
+    "Cenoura com Calabresa": "🥕",
+    #########################
+
+    "Pastel de Presunto e Muçarela": "🍖",
+    "Pastel de Carne": "🍖",
+    "Pastel de Calabresa": "🍖",
+    "Rondelli de Presunto e Muçarela": "🍖",
+    "Rondelli de Carne": "🍖",
+    "Canelone de Presunto e Muçarela": "🍖",
+    "Canelone de Carne": "🍖",
+    "Lasanha de Presunto e Muçarela": "🍖",
+    "Lasanha de Carne": "🍖",
+    #########################
+
+    "Milho Refogado": "🌽",
+    "Creme de Milho": "🌽",
+    "Milho Cozido": "🌽",
+    "Milho na Espiga": "🌽",
+    #########################
+
+    "Escondidinho de Frango": "🐓",
+    #########################
+
+
+    "Maionese": "🥗",
+    "Tabule": "🥗",
+    "Salpicão": "🥗",
+    "Dakota": "🥗",
+    "Batatonese": "🥗",
+    "Batata Verde": "🥗",
+    "Batata Temperada": "🥗",
+    "Salada Caprese": "🥗",
+    "Repolhonese": "🥗",
+    #########################
+}
 
 # take input from the user for each dish and add it to the appropriate list
-for section in sections:
+for section_name, section in sections.items():
     while True:
-        dish = input("Enter a dish for a section or 'next' to proceed: ")
+        dish = input(f"Enter a dish for {section_name} or 'next' to proceed: ")
         if dish == "next": 
             break
         else:
-            section.append("🍚" + dish if section is rice else "🥘" + dish if section is beans else "🥩" + dish if section is meats else "😋" + dish if section is sides else "🥗" + dish)
+            emoji = emoji_map.get(dish, "😋") # use the corresponding emoji, or a default one if not found
+            section.append(f"{emoji}{dish}")
 
 # print the menu 
-print("Bom dia!\nSegue o cardápio para marmitex e marmita:")
+print("Bom dia!\n\nSegue o cardápio para marmitex e marmita:")
 
 print("\n".join(rice))
 print("\n".join(beans))
